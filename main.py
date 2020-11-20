4c1b43070b4eefe43e515e2d83f3d9772391af52
Enter <yannick.katambo@outlook.com>
2020-11-20 23:12:39 +0200

    local website done

--- main.py
+++ main.py
@@ -5,8 +5,17 @@ with open('home.html', 'r') as html_file:
 
     soup = BeautifulSoup(content, 'lxml')
     # print(soup.prettify())
-    courses_html_tags = soup.find_all('h5')
+    # courses_html_tags = soup.find_all('h5')
 
-    for course in courses_html_tags:
-        print(course.text)
+    # for course in courses_html_tags:
+    #    print(course.text)
 
+    course_card = soup.find_all('div',class_='card')
+    for course in course_card:
+        course_name = course.h5.text
+        course_price = course.a.text.split()[2]
+
+        # print(course_name)
+        # print(course_price)
+
+        print(f'{course_name} costs {course_price}')
